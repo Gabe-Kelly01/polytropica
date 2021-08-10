dd5abd80985c33d1ffa6d4a898461b2c7eb39da4
Gabe Kelly <kellygj@whitman.edu>
2021-08-10 14:49:45 -0700

    Creating files and directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='polytropica',
+    packages=find_packages(include=['polytropica']),
+    version='0.0.1',
+    description='A Python 3 library for working with tropical polynomials',
+    author='Gabriel Kelly',
+    license='MIT',
+)
